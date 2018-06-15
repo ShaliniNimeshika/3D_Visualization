c16bbeb114f67ee145e1d20c07b956d76744387d
Unknown <nimeshika94@gmail.com>
2018-06-15 18:16:01 +0530

    login - backend

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -1,9 +1,45 @@
 <?php 
-class Auth_model extends CI_Model{
+class Home_model extends CI_Model{
 
 	public function __construct(){
 
 		parent::__construct();
 	}
 	
+
+	public function login($data){
+		$condition = "email =" . "'" . $data['uemail'] . "' AND " . "password =" . "'" . $data=['password'] . "'";
+		$this->db->SELECT('*');
+		$this->db->FROM('user');
+		$this->db->WHERE($condition);
+		$this->db->limit(1);
+
+		$query = $this->db->get();
+
+		if ($query->num_rows() == 1) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
+	public function search_user($user_email){
+
+		$condition = "email =" . "'" . $user_email . "'";
+		$this->db->SELECT('*');
+		$this->db->FROM('user');
+		$this->db->WHERE($condition);
+		$this->db->limit(1);
+
+		$query = $this->db->get();
+
+		if ($query->num_rows() == 1) {
+			return $query->result();
+		} else {
+			return false;
+		}		
+	}
+
+
 }
