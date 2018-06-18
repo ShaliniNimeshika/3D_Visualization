11934789e2e1b5e052d9ad6d18c39e625a34881c
Unknown <nimeshika94@gmail.com>
2018-06-18 21:02:17 +0530

    solve php error

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,21 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-session_start(); //we need to start session in order to access it through CI*/
+
 
 class Home extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-
-		// Load form helper library
-		$this->load->helper('form');
-
-		// Load form validation library
-		$this->load->library('form_validation');
-
-		// Load session library
-		$this->load->library('session');
 
 		// Load database
 		$this->load->model('Home_model');
